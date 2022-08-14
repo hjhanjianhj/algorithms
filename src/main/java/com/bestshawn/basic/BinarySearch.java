08cd82f11921a0ac57d52cff8bd057129a8eaf64
Paul Han <hjhanjianhj@163.com>
2022-08-15 01:47:17 +0800

    二分法 & 位运算

--- src/main/java/com/bestshawn/basic/BinarySearch.java
+++ src/main/java/com/bestshawn/basic/BinarySearch.java
@@ -27,4 +27,81 @@ public class BinarySearch {
 
         return false;
     }
+
+    /**
+     * 给定一个有序数组，查找满足arr[i]>=value最左边i的值
+     * @param arr
+     * @param value
+     * @return
+     */
+    public int nearestLeftIndex(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = 0;
+        int index = -1;
+
+        while (left <= right) {
+            mid = left + ((right - left) >> 1);
+            if (arr[mid] >= value) {
+                index = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return index;
+    }
+
+    public int nearestRightIndex(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = 0;
+        int index = -1;
+        while (left <= right) {
+            mid = left + ((right - left) >> 1);
+            if (arr[mid] <= value) {
+                left = mid + 1;
+                index = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return index;
+    }
+
+    /**
+     * 返回数组中的局部最小数的index，局部最小定义arr[i-1]>arr[i]<arr[i+1]
+     * arr数组特色要求：相邻的两个数不相等arr[i-1] != arr[i]
+     * @param arr
+     * @return
+     */
+    public int getLocalMin(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        if (arr.length == 1 || arr[0] < arr[1]) {
+            return 0;
+        }
+        if (arr[arr.length - 1] < arr[arr.length - 2]){
+            return arr.length - 1;
+        }
+
+        int left = 1;
+        int right = arr.length - 2;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + ((right - left) >> 1);
+            if (arr[mid - 1] < arr[mid]) {
+                right = mid - 1;
+            } else if (arr[mid + 1] < arr[mid]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return left;
+    }
 }
--- src/test/java/com/bestshawn/basic/BinarySearchTest.java
+++ src/test/java/com/bestshawn/basic/BinarySearchTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BinarySearchTest {
     private BinarySearch binarySearch;
@@ -28,6 +27,96 @@ public class BinarySearchTest {
             int expectIndex = Arrays.binarySearch(nums, num);
             assertEquals(expectIndex >= 0, ret);
         }
+    }
+
+    @Test
+    public void test_nearestLeftIndex() {
+        int maxLength = 100;
+        int maxValue = 1000;
+        for (int i = 0; i < 10000; i++) {
+            int[] nums = generateData(maxLength, maxValue);
+            int num = (int) ((maxValue + 1) * Math.random());
+            Arrays.sort(nums);
+            int ret = binarySearch.nearestLeftIndex(nums, num);
+            int except = nearestLeftIndex(nums, num);
+            assertEquals(except, ret);
+        }
+    }
+
+    @Test
+    public void test_nearestRightIndex() {
+        int maxLength = 100;
+        int maxValue = 1000;
+        for (int i = 0; i < 10000; i++) {
+            int[] nums = generateData(maxLength, maxValue);
+            int num = (int) ((maxValue + 1) * Math.random());
+            Arrays.sort(nums);
+            int ret = binarySearch.nearestRightIndex(nums, num);
+            int except = nearestRightIndex(nums, num);
+            if (ret != except) {
+                System.out.println(nums);
+                System.out.println(num);
+                assertTrue(false);
+            }
+            assertEquals(except, ret);
+        }
+    }
+
+    @Test
+    public void test_getLocalMin() {
+        int maxLength = 100;
+        int maxValue = 1000;
+        for (int i = 0; i < 10000; i++) {
+            int[] nums = generateNearNotEqualsData(maxLength, maxValue);
+            int ret = binarySearch.getLocalMin(nums);
+            assertGetMinLocalMin(nums, ret);
+        }
+    }
+
+    private void assertGetMinLocalMin(int[] nums, int index) {
+        if (nums.length <=1) {
+            return;
+        }
+        if (index == 0) {
+            assertTrue(nums[index] < nums[index + 1]);
+            return;
+        }
+        if (index == nums.length - 1) {
+            assertTrue(nums[index] < nums[index - 1]);
+            return;
+        }
+
+        assertTrue(nums[index] < nums[index + 1] && nums[index] < nums[index - 1]);
+    }
+
+    private int[] generateNearNotEqualsData(int maxLength, int maxValue) {
+        int length = (int)(Math.random() * (maxLength + 1));
+        int[] nums = new int[length];
+        for (int i = 0; i < length; i++) {
+            do {
+                nums[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            } while (i > 0 && nums[i-1] == nums[i]);
+        }
+        return nums;
+    }
+
+    private int nearestRightIndex(int[] nums, int num) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] <= num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int nearestLeftIndex(int[] nums, int num) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= num) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     private int[] generateData(int maxLength, int maxValue) {
